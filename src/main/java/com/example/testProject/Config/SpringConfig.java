4aa88008cb32a56535b6d988f651f50267108d2a
ranme <qkralfks3@naver.com>
2025-02-18 18:45:01 +0900

    Fix : html JS수정

--- src/main/java/com/example/testProject/Config/SpringConfig.java
+++ src/main/java/com/example/testProject/Config/SpringConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringConfig implements WebMvcConfigurer {
 
+    /**
+     * 이미지 경로 외부로 설정
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/product-images/**").addResourceLocations("file:///C:/product-images/");
