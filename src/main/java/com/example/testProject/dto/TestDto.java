978b1f04cb44ef2502c0e9b8a616acc8708f597d
ranme <qkralfks3@naver.com>
2025-02-18 17:23:07 +0900

    Fix : 정규식 메세지 오타 수정

--- src/main/java/com/example/testProject/dto/TestDto.java
+++ src/main/java/com/example/testProject/dto/TestDto.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class TestDto {
 
     @Pattern(regexp = "^[a-z]{1}[a-z0-9]{5,10}$"
-            ,message = "아이디는 영문자와 숫자조합으로 5~10자리여야합니다.")
+            ,message = "아이디는 영문자와 숫자조합으로 5~11자리여야합니다.")
     @NotBlank(message = "아이디는 필수 정보입니다.")
     private String userId;
 
