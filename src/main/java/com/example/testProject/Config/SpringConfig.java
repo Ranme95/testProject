a5af28018f78682679eb8f0a770d1e8fe350a61d
ranme <qkralfks3@naver.com>
2025-02-21 17:18:05 +0900

    Fix : 엔티티 변경 감지 안되던 문제 해결

--- src/main/java/com/example/testProject/Config/SpringConfig.java
+++ src/main/java/com/example/testProject/Config/SpringConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,4 +31,5 @@ public class SpringConfig implements WebMvcConfigurer {
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
 }
--- src/main/java/com/example/testProject/Controller/MemberController.java
+++ src/main/java/com/example/testProject/Controller/MemberController.java
@@ -22,6 +22,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
